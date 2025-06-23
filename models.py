f3b3287a8594a84d3b53ad64a46d209adc749655
Benicio <benicioferrer@gmail.com>
2025-06-23 02:50:10 -0300

    Se arreglaron errores

--- models.py
+++ models.py
@@ -1,6 +1,7 @@
 from sqlalchemy import Column, Integer, String, ForeignKey
 from sqlalchemy.orm import relationship
 from database import Base
+from sqlalchemy import DateTime
 
 class User(Base):
     __tablename__ = "users"
